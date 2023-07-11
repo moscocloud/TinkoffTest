f4152fd21f9f07d813e2d69cf4cac599d1b2a7a3
AntonChadov <skochchad97@gmail.com>
2023-07-11 12:16:06 +0300

    fix

--- src/main/java/managers/DriverManager.java
+++ src/main/java/managers/DriverManager.java
@@ -8,15 +8,15 @@ public class DriverManager {
     private WebDriver driver;
 
 
-
     public static DriverManager getInstance() {
         if (INSTANCE == null) {
             INSTANCE = new DriverManager();
         }
-            return INSTANCE;
+        return INSTANCE;
     }
+
     public WebDriver getDriver() {
-        if(driver == null) {
+        if (driver == null) {
             initDriver();
         }
         return driver;
@@ -27,103 +27,9 @@ public class DriverManager {
     }
 
     public void quitDriver() {
-        if(driver != null) {
+        if (driver != null) {
             driver.quit();
-            driver=null;
+            driver = null;
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    /**
-//     * Выбор элимента и клик по элементу. Выбор осущетвляется при помощи Xpath
-//     */
-//    public void clickElement(String xpath) {
-//        instance().findElement(By.xpath(xpath)).click();
-//
-//    }
-//
-//    /**
-//     * Выбор элимента и клик по элементу. Выбор осущетвляется при помощи Css
-//     */
-//    public void clickElementByCss(String cssSelector) {
-//        instance().findElement(By.cssSelector(cssSelector)).click();
-//    }
-//
-//    /**
-//     * Поиск элемента по Xpath. Возвращает элемент типа "WebElement"
-//     */
-//    public WebElement findXpath(String xpath) {
-//        return instance().findElement(By.xpath(xpath));
-//    }
-//
-//    /**
-//     * Поиск элемента по Css. Возвращает элемент типа "WebElement"
-//     */
-//    public WebElement findCss(String cssSelector) {
-//        return instance().findElement(By.cssSelector(cssSelector));
-//    }
-//
-//    /**
-//     * Ввод в поле значения. Метод принимает два параметра строку xpath и строку value.
-//     * Первый параметр указывает путь до элемента в формате Xpath. Второй параметр
-//     * передает строку которую нужно вставать в поле.
-//     * <p>
-//     * Метод сначала кликает по элементу. Потом очищает элемент, после вставляет значение
-//     * переданое вторым параметром и в конце сверяет значение поля после написания с значением
-//     * которое передали
-//     */
-//    public void inputField(String xpath, String value) {
-//        WebElement element = instance().findElement((By.xpath(xpath)));
-//        element.click();
-//        element.clear();
-//        element.sendKeys(value);
-//        Assertions.assertEquals(value, value, "Поле было введено некорректно");
-//    }
-//
-//    /**
-//     * Выполяет ту же функциональность что и метод inputField, но находит элемент по css селектору.
-//     */
-//    public void inputFieldByCssSelector(String cssSelector, String value) {
-//        WebElement element = instance().findElement((By.cssSelector(cssSelector)));
-//        element.click();
-//        element.clear();
-//        element.sendKeys(value);
-//        Assertions.assertEquals(value, value, "Поле было введено некорректно");
-//    }
-//}
-
